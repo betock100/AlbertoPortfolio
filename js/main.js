770d7fc2d2206b4163affde43acfba9ceaa11e05
Alberto Tuesta <betock100@yahoo.com>
2025-01-27 19:27:54 -0500

    general fixing

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const twoSquares = document.querySelector(".twoSquares");
+  const twoSquaresList = document.querySelectorAll(".twoSquares");
 
   //1000px min screen height
-
   if (window.innerHeight >=1000) {
-    twoSquares.querySelector(".squareL").style.animationPlayState = "running";
-    twoSquares.querySelector(".squareR").style.animationPlayState = "running";
+    twoSquaresList.forEach((twoSquares) => {
+      twoSquares.querySelector(".squareL").style.animationPlayState = "running";
+      twoSquares.querySelector(".squareR").style.animationPlayState = "running";
+
+    });    
 
   } else { 
     //smaller screens
     document.addEventListener("scroll", () => {
-        const twoSquares = document.querySelector(".twoSquares");
+      twoSquaresList.forEach((twoSquares) => {
+        //const twoSquares = document.querySelector(".twoSquares");
         const rect = twoSquares.getBoundingClientRect();
-      
+
         if (rect.top < window.innerHeight && rect.bottom > 0) {
           twoSquares.querySelector(".squareL").style.animationPlayState = "running";
           twoSquares.querySelector(".squareR").style.animationPlayState = "running";
         }
 
+      });
     });
-
-  }
-  
-})
+  }  
+});
